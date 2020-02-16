395205377087085a8d6615df25ef7f9ce5effe54
gouravthakur39 <gourav.thakur39@gmail.com>
2020-02-16 23:19:34 +0530

    save articles to mongoDB

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express')
 const path = require('path')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 
 mongoose.connect('mongodb://localhost/nodekb');
 let db = mongoose.connection;
@@ -27,6 +28,15 @@ let Article = require('./models/article');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// body parser middleware
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+
+
 
 const port = 3000
 
@@ -49,7 +59,29 @@ app.get('/', (req, res) => {
 app.get('/articles/add', (req, res) => 
     res.render('add_article', {
         title: 'Add Article'
-    }))
+    })
+);
+
+// add post submit route
+app.post('/articles/add', (req, res) => {
+    let article = new Article();
+
+    article.title = req.body.title;
+    article.author = req.body.author;
+    article.body = req.body.body;
+
+    article.save(err => {
+        if(err) {
+            console.log(err);
+            return;
+        } else {
+            res.redirect('/');
+        }
+    });
+
+});
+
+
 
 //start server
 app.listen(port, () => 
